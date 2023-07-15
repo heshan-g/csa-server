cf13ad8944f825a672183e0cff9c8a7df9cc8d03
Heshan <heshan.ua@gmail.com>
2023-07-15 16:53:52 +0530

    Add mongoose and configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,10 @@
 const config = {
   application: {
     port: parseInt(process.env.PORT as string),
-  }
+  },
+  database: {
+    connectionString: process.env.DATABASE_CONNECTION_STRING as string,
+  },
 }
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import 'dotenv/config';
-import config from "./config/config";
-import { createServer } from "./server";
+import config from './config/config';
+import { createServer } from './server';
+import mongoose from 'mongoose';
 
 const app = createServer();
 
 const port = config.application.port;
+const connectionString = config.database.connectionString;
 
 app
-  .listen(port, () => {
+  .listen(port, async () => {
     console.log(`Server is up and listening on port ${port}`);
+
+    try {
+      await mongoose.connect(connectionString);
+      console.log('Database connection successful');
+    } catch (err) {
+      console.error('Error connecting to DB.', err);
+    }
   })
   .on('error', (err) => {
     console.error('Error starting the server.', err);
